e8bf9597ac0e8ad8fbe3fb3a15103e3f2b32f637
cccg03 <cccg03@gmail.com>
2017-04-13 15:58:12 -0600

    Update CameraController.cs
    formatting

--- Roll-a-Ball/Assets/Scripts/CameraController.cs
+++ Roll-a-Ball/Assets/Scripts/CameraController.cs
@@ -2,21 +2,26 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+// Keeps the camera transform centered on the ball 
+
 public class CameraController : MonoBehaviour {
 	
 	public GameObject player;
 	
 	private Vector3 offset;
 	
-	// Use this for initialization
 	void Start () {
-		offset = transform.position - player.transform.position;
+		
+		offset = transform.position - player.transform.position;	
+	
 	}
 	
-	// Update is called once per frame
+// Updates frame only after everything has been rendered (hence the "Late")	
+	
 	void LateUpdate () {
-		transform.position = player.transform.position + offset;
 		
-		}
+		transform.position = player.transform.position + offset;
+	
+	}
 
 }
