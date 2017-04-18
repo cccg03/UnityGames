4308ab97e6b2c8a87c85cd2eadf0da37c4f9c1c7
Casey Garland <cjgarland@xactware.com>
2017-04-18 16:47:53 -0600

    start working on Laser Defender

---
+++ Laser Defender/Laster Defender/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    public float speed = 20.0f;
+    private float padding = .5f;
+
+    float xmin;
+    float xmax;
+
+    // Use this for initialization
+    void Start() {
+        float distance = transform.position.z - Camera.main.transform.position.z;
+        Vector3 leftmost = Camera.main.ViewportToWorldPoint(new Vector3(0,0,distance));
+        Vector3 rightmost = Camera.main.ViewportToWorldPoint(new Vector3(1,0, distance));
+        xmin = leftmost.x + padding;
+        xmax = rightmost.x - padding;
+    }
+
+    // Update is called once per frame
+    void Update() {
+        if (Input.GetKey(KeyCode.LeftArrow)) { 
+
+            transform.position += Vector3.left * speed * Time.deltaTime;
+
+    } else if (Input.GetKey(KeyCode.RightArrow)) {
+
+            transform.position += Vector3.right* speed * Time.deltaTime;
+
+        }
+
+        // restrict player's gamespace
+        float newX = Mathf.Clamp(transform.position.x, xmin, xmax);
+        transform.position = new Vector3(newX, transform.position.y, transform.position.z);
+
+    }
+}
